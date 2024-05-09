2488b05f1931822888015b0b6f4d596137989527
Xen <luisgpm98@gmail.com>
2024-05-09 03:33:30 -0600

    Primer commit

---
+++ lua/config/settings.lua
@@ -0,0 +1,9 @@
+vim.o.number = true
+vim.o.relativenumber = true
+vim.g.mapleader = " "
+vim.opt.smarttab = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+
